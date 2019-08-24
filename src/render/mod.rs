eb37a96018f42e82e20a2e7b9bcfffe71ce31dca
Chris Patuzzo <chris@patuzzo.co.uk>
2019-08-24 17:52:23 +0100

    Remove the feed module

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,11 +1,8 @@
 use specs::prelude::*;
 use web_sys::WebGlRenderingContext as GL;
-use crate::resources::{*, WebGlContext};
+
+use crate::resources::{*, shader_types::*};
 use crate::components::*;
-
-mod feed;
-
-use feed::*;
 
 pub struct Render;
 
@@ -47,4 +44,18 @@ impl<'a> System<'a> for Render {
             context.draw_arrays(GL::TRIANGLES, 0, elements as i32);
         }
     }
+}
+
+pub fn feed_attribute(context: &GL, program: &ShaderProgram, name: &str, buffer: &WebGlBuffer, size: i32) {
+    let location = program.attribute_location(name);
+
+    context.enable_vertex_attrib_array(location);
+    context.bind_buffer(GL::ARRAY_BUFFER, Some(&buffer));
+    context.vertex_attrib_pointer_with_i32(location, size, GL::FLOAT, false, 0, 0);
+}
+
+pub fn feed_uniform(context: &GL, program: &ShaderProgram, name: &str, matrix: &[f32]) {
+    let location = program.uniform_location(name);
+
+    context.uniform_matrix4fv_with_f32_array(Some(&location), false, matrix);
 }
